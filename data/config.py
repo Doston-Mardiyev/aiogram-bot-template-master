615f66b77a1b8ef9820c362529e3069236da62d7
Doston <dmardiyev96@gmail.com>
2021-12-05 20:10:27 +0500

    commit

--- data/config.py
+++ data/config.py
@@ -1,30 +1,30 @@
-# from environs import Env
+from environs import Env
 
-# # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = ["1395141"]  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
-# CHANNELS = ["-1001644108862"]
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = ["1395141"]  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
+CHANNELS = ["-1001644108862"]
 
-# #======db info ===========
-# DB_USER = env.str("DB_USER")
-# DB_PASS = env.str("DB_PASS")
-# DB_NAME = env.str("DB_NAME")
-# DB_HOST = env.str("DB_HOST")
+#======db info ===========
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
 
 
-import os
+# import os
 
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = str(os.environ.get("ADMINS"))
-IP = str(os.environ.get("IP"))
-CHANNELS = str(os.environ.get("CHANNELS"))
+# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+# ADMINS = str(os.environ.get("ADMINS"))
+# IP = str(os.environ.get("IP"))
+# CHANNELS = str(os.environ.get("CHANNELS"))
 
-DB_USER = str(os.environ.get("DB_USER"))
-DB_PASS = str(os.environ.get("DB_PASS"))
-DB_NAME = str(os.environ.get("DB_NAME"))
-DB_HOST = str(os.environ.get("DB_HOST"))
+# DB_USER = str(os.environ.get("DB_USER"))
+# DB_PASS = str(os.environ.get("DB_PASS"))
+# DB_NAME = str(os.environ.get("DB_NAME"))
+# DB_HOST = str(os.environ.get("DB_HOST"))
